5fd5bce99915b721a8bdb04709e69d160ace1efe
ilan-uzan <ilanxuzan@gmail.com>
2025-03-30 13:51:17 +0300

    main

--- Week2/Week1/Day3/exercise3.py
+++
@@ -1,6 +0,0 @@
-#Accept a number from the user and print its multiplication table
-
-number = int(input("Enter a number: "))
-for i in range(1, 11):
-    print(f"{number} x {i} = {number
-    * i}")
--- Week2/Week1/Day3/exercises4.py
+++
@@ -1,7 +0,0 @@
-#Print the numbers from 1 to 10 using while loop
-
-a_number = 1
-
-while a_number < 11:
-    print(a_number)
-    a_number += 1
